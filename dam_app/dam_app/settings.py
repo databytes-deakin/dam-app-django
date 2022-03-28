e603ea6c78ff4bbe37fca57847bffc0c53b933ab
Mark Blashki <markblashki1@gmail.com>
2022-03-28 17:42:08 +1100

    add: hello world experimentation database get, create tests models

--- dam_app/dam_app/settings.py
+++ dam_app/dam_app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hello_world',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- dam_app/dam_app/urls.py
+++ dam_app/dam_app/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from hello_world import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create/', views.create_user),
+    path('users/<int:id>/', views.get_user, name='get_user'),
+    path('test/', views.test_view, name='test'),
 ]
